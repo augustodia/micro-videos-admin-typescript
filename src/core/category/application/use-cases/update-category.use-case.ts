1c00b3c3a09b4e14c3d8638c4b671d858d831ca3
Luiz Augusto <augustodia97@gmail.com>
2024-04-08 20:54:11 -0300

    🏗️ added NestJS

---
+++ src/core/category/application/use-cases/update-category.use-case.ts
@@ -0,0 +1,51 @@
+import { IUseCase } from '../../../shared/application/use-case.interface';
+import { NotFoundError } from '../../../shared/domain/errors/not-found.error';
+import { EntityValidationError } from '../../../shared/domain/errors/validation.error';
+import { Uuid } from '../../../shared/domain/value-objects/uuid.vo';
+import { Category } from '../../domain/category.entity';
+import { ICategoryRepository } from '../../domain/category.repository';
+import { CategoryOutputMapper } from './common/category-output';
+
+export type UpdateCategoryInput = {
+  id: string;
+  name?: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export type UpdateCategoryOutput = {
+  id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+};
+
+export class UpdateCategoryUseCase
+  implements IUseCase<UpdateCategoryInput, UpdateCategoryOutput>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: UpdateCategoryInput): Promise<UpdateCategoryOutput> {
+    const uuid = new Uuid(input.id);
+    const category = await this.categoryRepo.findById(uuid);
+
+    if (!category) {
+      throw new NotFoundError(input.id, Category);
+    }
+
+    category.update({
+      name: input.name,
+      description: input.description,
+      is_active: input.is_active,
+    });
+
+    if (category.notification.hasErrors()) {
+      throw new EntityValidationError(category.notification.toJSON());
+    }
+
+    await this.categoryRepo.update(category);
+
+    return CategoryOutputMapper.toOutput(category);
+  }
+}
--- src/shared/domain/entity.ts
+++
@@ -1,9 +0,0 @@
-import { ValueObject } from "./value-object";
-import { Notification } from "./validators/notification";
-export abstract class Entity {
-
-  notification: Notification = new Notification();
-
-  abstract get entity_id(): ValueObject
-  abstract toJSON(): any;
-}
