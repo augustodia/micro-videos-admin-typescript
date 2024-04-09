f88de4bbc3c14e1705a79f48ab82e9071d0be808
Luiz Augusto <augustodia97@gmail.com>
2024-04-09 14:01:40 -0300

    🏗️ added ConfigModule and DatabaseModule

---
+++ src/categories/categories.controller.spec.ts
@@ -0,0 +1,33 @@
+import { ConfigModule } from './../config/config.module';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesController } from './categories.controller';
+import { DatabaseModule } from '../database/database.module';
+import { CategoriesModule } from './categories.module';
+
+describe('CategoriesController', () => {
+  let controller: CategoriesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
+      /* PODE SER FEITO ASSIM, CASO NÃO QUEIRA MONTAR O MÓDULO INTEIRO PARA TESTAR O CONTROLLER
+       * imports: [DatabaseModule, SequelizeModule.forFeature([CategoryModel])],
+       * controllers: [CategoriesController],
+       * providers: [
+       *   {
+       *     provide: CategorySequelizeRepository,
+       *     useFactory: (categoryModel: typeof CategoryModel) =>
+       *       new CategorySequelizeRepository(categoryModel),
+       *     inject: [getModelToken(CategoryModel)],
+       *   },
+       * ],
+       */
+    }).compile();
+
+    controller = module.get<CategoriesController>(CategoriesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
