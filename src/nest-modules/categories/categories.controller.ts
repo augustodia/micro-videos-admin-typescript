5521627944e51ce9a99f63149ac1117c64d89354
Luiz Augusto <augustodia97@gmail.com>
2024-04-09 20:37:28 -0300

    ✨ implements Create Category route

--- src/nest-modules/categories/categories.controller.ts
+++ src/nest-modules/categories/categories.controller.ts
@@ -7,16 +7,30 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
+
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '../../core/category/infra/db/sequelize/category-sequelize.repository';
+
+import { UpdateCategoryUseCase } from '../../core/category/application/use-cases/update-category/update-category.use-case';
+import { DeleteCategoryUseCase } from '../../core/category/application/use-cases/delete-category.use-case';
+import { GetCategoryUseCase } from '../../core/category/application/use-cases/get-category.use-case';
+import { ListCategoriesUseCase } from '../../core/category/application/use-cases/list-categories.use-case';
+import { CreateCategoryUseCase } from '../../core/category/application/use-cases/create-category/create-category.use-case';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private categoryRepo: CategorySequelizeRepository) {}
+  constructor(
+    private createUseCase: CreateCategoryUseCase,
+    private updateUseCase: UpdateCategoryUseCase,
+    private deleteUseCase: DeleteCategoryUseCase,
+    private getUseCase: GetCategoryUseCase,
+    private listUseCase: ListCategoriesUseCase,
+  ) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {}
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.createUseCase.execute(createCategoryDto);
+  }
 
   @Get()
   findAll() {}
