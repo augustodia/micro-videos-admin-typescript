4beac9b69c530b9dbf3576ebb1fffed67c6a35dc
Luiz Augusto <augustodia97@gmail.com>
2024-04-25 22:45:44 -0300

    ✅ unit test CategoryController

--- src/nest-modules/categories/__tests__/categories.controller.spec.ts
+++ src/nest-modules/categories/__tests__/categories.controller.spec.ts
@@ -1,33 +1,110 @@
-import { ConfigModule } from '../../config/config.module';
-import { Test, TestingModule } from '@nestjs/testing';
+import { UpdateCategoryInput } from '../../../core/category/application/use-cases/update-category/update-category.input';
+import { SortDirection } from '../../../core/shared/domain/repository/search-params';
 import { CategoriesController } from '../categories.controller';
-import { DatabaseModule } from '../../database/database.module';
-import { CategoriesModule } from '../categories.module';
+import {
+  CategoryPresenter,
+  CategoryCollectionPresenter,
+} from '../categories.presenter';
+import { CreateCategoryDto } from '../dto/create-category.dto';
+import {
+  MockCreateCategoryUseCase,
+  MockDeleteCategoryUseCase,
+  MockGetCategoryUseCase,
+  MockListCategoriesUseCase,
+  MockUpdateCategoryUseCase,
+  outputCreateUseCase,
+  outputDeleteUseCase,
+  outputGetUseCase,
+  outputListUseCase,
+  outputUpdateUseCase,
+} from './mocks/categories.usecases.mocks';
 
-describe('CategoriesController', () => {
+describe('CategoriesController Unit Tests', () => {
   let controller: CategoriesController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
-      /* PODE SER FEITO ASSIM, CASO NÃO QUEIRA MONTAR O MÓDULO INTEIRO PARA TESTAR O CONTROLLER
-       * imports: [DatabaseModule, SequelizeModule.forFeature([CategoryModel])],
-       * controllers: [CategoriesController],
-       * providers: [
-       *   {
-       *     provide: CategorySequelizeRepository,
-       *     useFactory: (categoryModel: typeof CategoryModel) =>
-       *       new CategorySequelizeRepository(categoryModel),
-       *     inject: [getModelToken(CategoryModel)],
-       *   },
-       * ],
-       */
-    }).compile();
+  const mockCreateUseCase = new MockCreateCategoryUseCase();
+  const mockUpdateUseCase = new MockUpdateCategoryUseCase();
+  const mockDeleteUseCase = new MockDeleteCategoryUseCase();
+  const mockGetUseCase = new MockGetCategoryUseCase();
 
-    controller = module.get<CategoriesController>(CategoriesController);
+  const mockListUseCase = new MockListCategoriesUseCase();
+
+  beforeEach(async () => {
+    controller = new CategoriesController(
+      mockCreateUseCase,
+      mockUpdateUseCase,
+      mockDeleteUseCase,
+      mockGetUseCase,
+      mockListUseCase,
+    );
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should creates a category', async () => {
+    const input: CreateCategoryDto = {
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+    };
+
+    const presenter = await controller.create(input);
+
+    expect(mockCreateUseCase.execute).toHaveBeenCalledWith(input);
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(outputCreateUseCase));
+  });
+
+  it('should updates a category', async () => {
+    const input: Omit<UpdateCategoryInput, 'id'> = {
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+    };
+
+    const presenter = await controller.update(outputUpdateUseCase.id, input);
+
+    expect(mockUpdateUseCase.execute).toHaveBeenCalledWith({
+      id: outputUpdateUseCase.id,
+      ...input,
+    });
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(outputUpdateUseCase));
+  });
+
+  it('should deletes a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+
+    const output = await controller.remove(id);
+
+    expect(controller.remove(id)).toBeInstanceOf(Promise);
+    expect(mockDeleteUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(outputDeleteUseCase).toStrictEqual(output);
+  });
+
+  it('should gets a category', async () => {
+    const presenter = await controller.findOne(outputGetUseCase.id);
+
+    expect(mockGetUseCase.execute).toHaveBeenCalledWith({
+      id: outputGetUseCase.id,
+    });
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(outputGetUseCase));
+  });
+
+  it('should list categories', async () => {
+    const searchParams = {
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc' as SortDirection,
+      filter: 'test',
+    };
+
+    const presenter = await controller.search(searchParams);
+
+    expect(presenter).toBeInstanceOf(CategoryCollectionPresenter);
+    expect(mockListUseCase.execute).toHaveBeenCalledWith(searchParams);
+    expect(presenter).toEqual(
+      new CategoryCollectionPresenter(outputListUseCase),
+    );
   });
 });
---
+++ src/nest-modules/categories/__tests__/mocks/categories.usecases.mocks.ts
@@ -0,0 +1,121 @@
+import {
+  CreateCategoryOutput,
+  CreateCategoryUseCase,
+} from '../../../../core/category/application/use-cases/create-category/create-category.use-case';
+import { DeleteCategoryUseCase } from '../../../../core/category/application/use-cases/delete-category.use-case';
+import {
+  GetCategoryOutput,
+  GetCategoryUseCase,
+} from '../../../../core/category/application/use-cases/get-category.use-case';
+import {
+  ListCategoriesOutput,
+  ListCategoriesUseCase,
+} from '../../../../core/category/application/use-cases/list-categories.use-case';
+import {
+  UpdateCategoryOutput,
+  UpdateCategoryUseCase,
+} from '../../../../core/category/application/use-cases/update-category/update-category.use-case';
+//#region Create
+const outputCreateUseCase: CreateCategoryOutput = {
+  id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+  name: 'Movie',
+  description: 'some description',
+  is_active: true,
+  created_at: new Date(),
+};
+
+class MockCreateCategoryUseCase extends CreateCategoryUseCase {
+  constructor() {
+    super(null as any);
+  }
+
+  execute = jest.fn().mockResolvedValue(outputCreateUseCase);
+}
+//#endregion
+
+//#region Update
+const outputUpdateUseCase: UpdateCategoryOutput = {
+  id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+  name: 'Movie',
+  description: 'some description',
+  is_active: true,
+  created_at: new Date(),
+};
+
+class MockUpdateCategoryUseCase extends UpdateCategoryUseCase {
+  constructor() {
+    super(null as any);
+  }
+
+  execute = jest.fn().mockResolvedValue(outputUpdateUseCase);
+}
+//#endregion
+
+//#region Delete
+const outputDeleteUseCase = undefined;
+
+class MockDeleteCategoryUseCase extends DeleteCategoryUseCase {
+  constructor() {
+    super(null as any);
+  }
+
+  execute = jest.fn().mockResolvedValue(outputDeleteUseCase);
+}
+//#region
+
+//#region Get
+const outputGetUseCase: GetCategoryOutput = {
+  id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+  name: 'Movie',
+  description: 'some description',
+  is_active: true,
+  created_at: new Date(),
+};
+
+class MockGetCategoryUseCase extends GetCategoryUseCase {
+  constructor() {
+    super(null as any);
+  }
+
+  execute = jest.fn().mockResolvedValue(outputGetUseCase);
+}
+//#endregion
+
+//#region List
+const outputListUseCase: ListCategoriesOutput = {
+  items: [
+    {
+      id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    },
+  ],
+  current_page: 1,
+  last_page: 1,
+  per_page: 1,
+  total: 1,
+};
+
+class MockListCategoriesUseCase extends ListCategoriesUseCase {
+  constructor() {
+    super(null as any);
+  }
+
+  execute = jest.fn().mockResolvedValue(outputListUseCase);
+}
+//#endregion
+
+export {
+  MockCreateCategoryUseCase,
+  MockUpdateCategoryUseCase,
+  MockDeleteCategoryUseCase,
+  MockGetCategoryUseCase,
+  MockListCategoriesUseCase,
+  outputCreateUseCase,
+  outputUpdateUseCase,
+  outputDeleteUseCase,
+  outputGetUseCase,
+  outputListUseCase,
+};
