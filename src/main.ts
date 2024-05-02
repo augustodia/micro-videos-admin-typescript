3060ec075ec684175c98817cc490235493385d3e
Luiz Augusto <augustodia97@gmail.com>
2024-05-01 21:29:11 -0300

    ✨ add error filters

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { WrapperDataInterceptor } from './nest-modules/interceptors/wrapper-data/wrapper-data.interceptor';
-import { NotFoundFilter } from './nest-modules/filters/not-found-error/not-found-error.filter';
+import { NotFoundErrorFilter } from './nest-modules/filters/not-found-error/not-found-error.filter';
+import { EntityValidationErrorFilter } from './nest-modules/filters/entity-validation-error/entity-validation-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,8 @@ async function bootstrap() {
     )
     .useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
     .useGlobalInterceptors(new WrapperDataInterceptor())
-    .useGlobalFilters(new NotFoundFilter());
+    .useGlobalFilters(new NotFoundErrorFilter())
+    .useGlobalFilters(new EntityValidationErrorFilter());
 
   await app.listen(3000);
 }
