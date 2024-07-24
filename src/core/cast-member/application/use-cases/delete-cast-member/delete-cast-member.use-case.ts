69f6127a3665132f2260eb390ba1f4aa95c58b4e
Luiz Augusto <augustodia97@gmail.com>
2024-07-24 14:40:52 -0300

    ✅  Delete CastMember UseCase tests

---
+++ src/core/cast-member/application/use-cases/delete-cast-member/delete-cast-member.use-case.ts
@@ -0,0 +1,20 @@
+import { IUseCase } from '../../../../shared/application/use-case.interface';
+import { Uuid } from '../../../../shared/domain/value-objects/uuid.vo';
+import { ICastMemberRepository } from '../../../domain/cast-member.repository';
+
+export type DeleteCastMemberInput = {
+  id: string;
+};
+
+type DeleteCastMemberOutput = void;
+
+export class DeleteCastMemberUseCase
+  implements IUseCase<DeleteCastMemberInput, DeleteCastMemberOutput>
+{
+  constructor(private categoryRepo: ICastMemberRepository) {}
+
+  async execute(input: DeleteCastMemberInput): Promise<DeleteCastMemberOutput> {
+    const uuid = new Uuid(input.id);
+    await this.categoryRepo.delete(uuid);
+  }
+}
