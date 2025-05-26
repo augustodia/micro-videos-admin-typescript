c00c3c79492529050720fbe39e51e7c34ec1d4c7
Luiz Augusto <augustodia97@gmail.com>
2025-05-25 23:17:05 -0300

    **♻️ Refactor event handling and module structure**
    Moved `DomainEventMediator` to `EventModule` for better modularization. Adjusted `SharedModule` and introduced `UseCaseModule` in `AppModule` to streamline dependencies and responsibilities.

--- src/nest-modules/event-module/event.module.ts
+++ src/nest-modules/event-module/event.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { DomainEventMediator } from '@core/shared/domain/events/domain-event-mediator';
+import EventEmitter2 from 'eventemitter2';
 
+@Global()
 @Module({
   imports: [EventEmitterModule.forRoot()],
+  providers: [
+    {
+      provide: DomainEventMediator,
+      useFactory: (eventEmitter: EventEmitter2) => {
+        return new DomainEventMediator(eventEmitter);
+      },
+      inject: [EventEmitter2],
+    },
+  ],
+  exports: [DomainEventMediator],
 })
 export class EventModule {}
