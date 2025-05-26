7b489645fa99074c432616d69462e4d5756dea0e
Luiz Augusto <augustodia97@gmail.com>
2025-05-25 23:16:58 -0300

    🎉 Add UseCaseModule to provide ApplicationService
    Introduce a new UseCaseModule with a scoped ApplicationService. The service is registered with a factory and supports dependency injection for UnitOfWork and DomainEventMediator.

---
+++ src/nest-modules/use-case/use-case.module.ts
@@ -0,0 +1,22 @@
+import { Module, Scope } from '@nestjs/common';
+import { ApplicationService } from '@core/shared/application/application.service';
+import { IUnitOfWork } from '@core/shared/domain/repository/unit-of-work.interface';
+import { DomainEventMediator } from '@core/shared/domain/events/domain-event-mediator';
+
+@Module({
+  providers: [
+    {
+      provide: ApplicationService,
+      useFactory: (
+        uow: IUnitOfWork,
+        domainEventMediator: DomainEventMediator,
+      ) => {
+        return new ApplicationService(uow, domainEventMediator);
+      },
+      inject: ['UnitOfWork', DomainEventMediator],
+      scope: Scope.REQUEST,
+    },
+  ],
+  exports: [ApplicationService],
+})
+export class UseCaseModule {}
